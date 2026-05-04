68405dc878a329db91587facdd65c8547a257c78
jouleion <124351760+jouleion@users.noreply.github.com>
2026-05-04 16:50:44 +0200

    Encoder testbench

---
+++ assignment_04/Encoder_tb.v
@@ -0,0 +1,66 @@
+`include "Encoder.v" // Include YOUR entity.
+`timescale 1ns / 1ps  // Time unit = period, precision
+module TopEntity_tb;
+  reg FPGA_CLK1_50;
+  reg [3:0] SW;
+  reg [7:0] LED;
+
+  integer i;
+  integer j;
+
+  TopEntity dut (
+      .FPGA_CLK1_50(FPGA_CLK1_50),
+      .SW(SW),
+      .LED(LED)
+  );
+
+  // Generate clock signal
+  initial begin
+    FPGA_CLK1_50 = 0;
+    forever #1 FPGA_CLK1_50 = ~FPGA_CLK1_50;
+  end
+
+  // Testbench script
+  initial begin
+    $dumpfile("signals.vcd");  // Name of the signal dump file
+    $dumpvars(0, TopEntity_tb);  // Signals to dump
+
+    // Initialize inputs
+    SW = 4'b0000;
+
+    // Reset the system
+    SW[2] = 1;
+    #5;
+    SW[2] = 0;
+
+
+ // Simulate CW rotation (Clockwise)
+    for (j = 0; j < 10; j = j + 1) begin
+      for (i = 0; i < 4; i = i + 1) begin
+        case (i)
+          0: {SW[0], SW[1]} = 2'b00;
+          1: {SW[0], SW[1]} = 2'b10;
+          2: {SW[0], SW[1]} = 2'b11;
+          3: {SW[0], SW[1]} = 2'b01;
+        endcase
+        #10;
+      end
+    end
+
+    // Simulate CCW rotation (Counterclockwise)
+    for (j = 0; j < 5; j = j + 1) begin
+      for (i = 0; i < 4; i = i + 1) begin
+        case (i)
+          0: {SW[0], SW[1]} = 2'b00;
+          1: {SW[0], SW[1]} = 2'b01;
+          2: {SW[0], SW[1]} = 2'b11;
+          3: {SW[0], SW[1]} = 2'b10;
+        endcase
+        #10;
+      end
+    end
+
+    #500; // Allow time for simulation
+    $finish;  // End simulation
+  end
+endmodule
